c945e8a99ef1f96f40aed98d90568702749b766c
cutecactus-hub <themanedwolf999@gmail.com>
2025-07-15 19:35:11 +0500

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let marks = ['google', 'sectum', 'veclar' , 'magna' , 'techno']
 
+//task 1
 companies.shift();
+
+
+// task 2
 
 companies.splice(2,1,'splice' );
 
+// task 3
 companies.push('amazon');
